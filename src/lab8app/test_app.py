907c68be4d19f25037a452fdbcdfac5524184d65
Henry-Hu-25 <qhu16@dons.usfca.edu>
2025-04-29 00:00:14 -0700

    test

---
+++ src/lab8app/test_app.py
@@ -0,0 +1,18 @@
+import requests
+
+url = "http://127.0.0.1:8000/predict"
+
+payload = {
+    "odometer": 50000,
+    "age": 26  # Make sure this matches your Pydantic model (not "age")
+}
+
+headers = {
+    "accept": "application/json",
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print("Status code:", response.status_code)
+print("Response:", response.json())
